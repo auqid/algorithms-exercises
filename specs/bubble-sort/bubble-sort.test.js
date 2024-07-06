28cd5518bf989eed79f54a115f3f6729ec7b26ff
auqid <auqidirfan100@gmail.com>
2024-07-06 21:00:55 +0530

    insertion sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,18 +12,19 @@
 
 function bubbleSort(nums) {
   let swapped = false;
-  do {
-    swapped = false
-    for (i=0;i<nums.length; i++){
-      if(nums[i] > nums[i+1]){
-        const temp = nums[i]
-        nums[i] = nums[i+1]
-        nums [i+1] = temp
+  do{
+    swapped = false;
+    for(let i=0;i<nums.length;i++){
+      if(nums[i]>nums[i+1]){
+        let temp = nums[i]
+        nums[i] = nums[i + 1]
+        nums[i+1] = temp
         swapped = true;
-        }
-       }
+      }
+    }
+   
   }while(swapped)
-   return nums
+    return nums
 }
 
 // unit tests
